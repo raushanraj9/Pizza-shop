d8bf68f6d129678f6b7b527aba6db44500648863
raushanraj9 <raushanr363@gmail.com>
2024-01-27 20:55:19 +0530

    created and added all the components required

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import MainSection from "./MainSection";
+import PizzaStageSection from "./PizzaStageSection";
+import PlaceOrder from "./PlaceOrder";
+import { Provider } from "react-redux";
+import store from "../redux/store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="p-2">
+        <h1 className="my-2 text-center font-bold">🍕 PizzaMint 🍕</h1>
+        <PlaceOrder />
+        <PizzaStageSection />
+        <MainSection />
+        <hr />
+        <h1 className="my-2 text-center font-bold">
+          Assignment by: Aman Verma
+        </h1>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
